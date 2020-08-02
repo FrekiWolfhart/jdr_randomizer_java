36d49fa498ed37403a15d8761565d55283965e8a
FrekiWolfhart <yoshii13@yahoo.fr>
2020-08-02 09:25:36 +0200

    PAssage en mode factory complet.

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,14 @@
 package src;
 
+import src.PersonnageFactory.Personnage;
+import src.PersonnageFactory.PersonnageEspece;
+import src.PersonnageFactory.PersonnageFactory;
+
 public class Main{
     public static void main(String[] args) {
-        Personnnage personnage = new Personnnage();
+        PersonnageFactory factory = new PersonnageFactory();
+        //Personnage personnage = factory.createPersonnage(PersonnageEspece.getRandomEspece());
+        Personnage personnage = factory.createPersonnage(PersonnageEspece.Humain);
         personnage.print();
     }
 }
